ea997cddcd72fed572f70231c4b81736cac5e33c
pdth112005-arch <pdth112005@gmail.com>
2026-06-23 09:16:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Portfolio.PhamDangThuHien/',
     plugins: [react(), tailwindcss()],
     publicDir: 'assets',
     resolve: {
